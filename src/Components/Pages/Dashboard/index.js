3967beb6e3c99a36b87a739bfe0827279457ce7e
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 15:48:56 +0700

    add title recent orders

--- src/Components/Pages/Dashboard/index.js
+++ src/Components/Pages/Dashboard/index.js
@@ -1,8 +1,8 @@
 import { Card, Space, Typography, Statistic, Table } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { DollarCircleOutlined, ShoppingFilled, UserOutlined } from '@ant-design/icons/lib/icons';
-import {getRecentOrders} from '../../../API'
-import  { useEffect, useState } from 'react';
+import { getRecentOrders } from '../../../API'
+import { useEffect, useState } from 'react';
 function Dashboard() {
     return (<div>
         <Typography.Title level={4}>Dashboard</Typography.Title>
@@ -10,40 +10,40 @@ function Dashboard() {
             <DashboardCard title={'Orders'}
                 value={12345}
                 icon={<ShoppingCartOutlined
-                    style={{ 
-                        color: 'green', 
-                        backgroundColor: 'rgba(0,255,0,0.25)' ,
-                        borderRadius:20,
-                        fontSize:24,
-                        padding:8
+                    style={{
+                        color: 'green',
+                        backgroundColor: 'rgba(0,255,0,0.25)',
+                        borderRadius: 20,
+                        fontSize: 24,
+                        padding: 8
                     }}
                 />}
             />
-            <DashboardCard title={'Customers'} value={532} icon={<UserOutlined 
-            style={{ 
-                color: 'purple', 
-                backgroundColor: 'rgba(0,255,255,0.25)' ,
-                borderRadius:20,
-                fontSize:24,
-                padding:8
-            }}/>} />
-            <DashboardCard title={'Inventory'} value={53632} icon={<ShoppingFilled style={{ 
-                color: 'blue', 
-                backgroundColor: 'rgba(0,0,255,0.25)' ,
-                borderRadius:20,
-                fontSize:24,
-                padding:8
-            }}/>} />
-            <DashboardCard title={'Revenue'} value={3563} icon={<DollarCircleOutlined style={{ 
-                color: 'red', 
-                backgroundColor: 'rgba(255,0,0,0.25)' ,
-                borderRadius:20,
-                fontSize:24,
-                padding:8
-            }}/>} />
+            <DashboardCard title={'Customers'} value={532} icon={<UserOutlined
+                style={{
+                    color: 'purple',
+                    backgroundColor: 'rgba(0,255,255,0.25)',
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8
+                }} />} />
+            <DashboardCard title={'Inventory'} value={53632} icon={<ShoppingFilled style={{
+                color: 'blue',
+                backgroundColor: 'rgba(0,0,255,0.25)',
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8
+            }} />} />
+            <DashboardCard title={'Revenue'} value={3563} icon={<DollarCircleOutlined style={{
+                color: 'red',
+                backgroundColor: 'rgba(255,0,0,0.25)',
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8
+            }} />} />
         </Space>
         <Space>
-            <RecentOrders/>
+            <RecentOrders />
         </Space>
     </div>);
 }
@@ -53,40 +53,43 @@ function DashboardCard({ title, value, icon }) {
         <Space direction='horizontal'>
             {icon}
             <Statistic title={title} value={value} />
-        </Space>        
+        </Space>
     </Card>)
 }
-function RecentOrders(){
+function RecentOrders() {
     const [dataSource, setDataSource] = useState([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-      setLoading(true);    
-      getRecentOrders().then(rs=>{
-        setDataSource(rs.products);
-        setLoading(false);
-      })
+        setLoading(true);
+        getRecentOrders().then(rs => {
+            setDataSource(rs.products);
+            setLoading(false);
+        })
     }, [])
-    
-    return <Table
-        columns={[
-            {
-                title:'Title',
-                dataIndex:'title'
-            },
-            {
-                title:'Price',
-                dataIndex:'discountedPrice'
-            },
-            {
-                title:'Qty',
-                dataIndex:'quantity'
-            }           
-        ]}
-        loading={loading}
-        dataSource={dataSource}
-        pagination={false}
-    ></Table>
+
+    return <>
+        <Typography.Text>Recent Orders</Typography.Text>
+        <Table
+            columns={[
+                {
+                    title: 'Title',
+                    dataIndex: 'title'
+                },
+                {
+                    title: 'Price',
+                    dataIndex: 'discountedPrice'
+                },
+                {
+                    title: 'Qty',
+                    dataIndex: 'quantity'
+                }
+            ]}
+            loading={loading}
+            dataSource={dataSource}
+            pagination={false}
+        ></Table>
+    </>
 }
 
 export default Dashboard;
