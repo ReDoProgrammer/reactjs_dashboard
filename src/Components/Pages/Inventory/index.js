a27879b9c90c5f0e5f68c4be7235626b4195f128
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 21:59:52 +0700

    set disable rating

--- src/Components/Pages/Inventory/index.js
+++ src/Components/Pages/Inventory/index.js
@@ -39,7 +39,7 @@ function Inventory() {
                     label: 'Rating',
                     dataIndex: 'rating',
                     render: rating=>{
-                        return <Rate value={rating} allowHalf/>
+                        return <Rate value={rating} allowHalf disabled/>
                     }
                 },
                 {
