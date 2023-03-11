ae28b0d87757bdfa091a0471cc7aa1ce526688d9
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 15:29:01 +0700

    design dashboard page

--- src/Components/Pages/Dashboard/index.js
+++ src/Components/Pages/Dashboard/index.js
@@ -1,8 +1,55 @@
-import {Typography} from 'antd';
+import { Card, Space, Typography, Statistic } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { DollarCircleOutlined, ShoppingFilled, UserOutlined } from '@ant-design/icons/lib/icons';
 function Dashboard() {
-    return ( <div>
+    return (<div>
         <Typography.Title level={4}>Dashboard</Typography.Title>
-    </div> );
+        <Space direction='horizontal'>
+            <DashboardCard title={'Orders'}
+                value={12345}
+                icon={<ShoppingCartOutlined
+                    style={{ 
+                        color: 'green', 
+                        backgroundColor: 'rgba(0,255,0,0.25)' ,
+                        borderRadius:20,
+                        fontSize:24,
+                        padding:8
+                    }}
+                />}
+            />
+            <DashboardCard title={'Customers'} value={532} icon={<UserOutlined 
+            style={{ 
+                color: 'purple', 
+                backgroundColor: 'rgba(0,255,255,0.25)' ,
+                borderRadius:20,
+                fontSize:24,
+                padding:8
+            }}/>} />
+            <DashboardCard title={'Inventory'} value={53632} icon={<ShoppingFilled style={{ 
+                color: 'blue', 
+                backgroundColor: 'rgba(0,0,255,0.25)' ,
+                borderRadius:20,
+                fontSize:24,
+                padding:8
+            }}/>} />
+            <DashboardCard title={'Revenue'} value={3563} icon={<DollarCircleOutlined style={{ 
+                color: 'red', 
+                backgroundColor: 'rgba(255,0,0,0.25)' ,
+                borderRadius:20,
+                fontSize:24,
+                padding:8
+            }}/>} />
+        </Space>
+    </div>);
+}
+
+function DashboardCard({ title, value, icon }) {
+    return (<Card>
+        <Space direction='horizontal'>
+            {icon}
+            <Statistic title={title} value={value} />
+        </Space>
+    </Card>)
 }
 
 export default Dashboard;
