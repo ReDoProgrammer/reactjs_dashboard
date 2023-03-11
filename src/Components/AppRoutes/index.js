0897158ea22f66bc95c5f8ca76a7819bc0e23c56
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 13:10:54 +0700

    fix BrowserRouter called in index.js

--- src/Components/AppRoutes/index.js
+++ src/Components/AppRoutes/index.js
@@ -1,18 +1,16 @@
-import {BrowserRoutes,Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Customer from '../Pages/Customer';
 import Dashboard from '../Pages/Dashboard';
 import Inventory from '../Pages/Inventory';
 import Order from '../Pages/Order';
 function AppRoutes() {
-    return ( 
-    <BrowserRoutes>
-    <Routes>
-        <Route path='/' element={<Dashboard/>}></Route>
-        <Route path='/customers' element={<Customer/>}></Route>
-        <Route path='/inventory' element={<Inventory/>}></Route>
-        <Route path='/orders' element={<Order/>}></Route>
-    </Routes>
-    </BrowserRoutes> 
+    return (
+        <Routes>
+            <Route path='/' element={<Dashboard />}></Route>
+            <Route path='/customers' element={<Customer />}></Route>
+            <Route path='/inventory' element={<Inventory />}></Route>
+            <Route path='/orders' element={<Order />}></Route>
+        </Routes>
     );
 }
 
