443838766417f8930b91c34dc7b90f3e8cb4fbed
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 16:09:19 +0700

    design dashboard with react chart

--- src/Components/Pages/Dashboard/index.js
+++ src/Components/Pages/Dashboard/index.js
@@ -3,6 +3,29 @@ import { ShoppingCartOutlined } from '@ant-design/icons';
 import { DollarCircleOutlined, ShoppingFilled, UserOutlined } from '@ant-design/icons/lib/icons';
 import { getRecentOrders } from '../../../API'
 import { useEffect, useState } from 'react';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
 function Dashboard() {
     return (<div>
         <Space size={20} direction="vertical">
@@ -45,6 +68,7 @@ function Dashboard() {
             </Space>
             <Space>
                 <RecentOrders />
+                <DasboardChart/>
             </Space>
         </Space>
     </div>);
@@ -65,7 +89,7 @@ function RecentOrders() {
     useEffect(() => {
         setLoading(true);
         getRecentOrders().then(rs => {
-            setDataSource(rs.products.splice(0,3));
+            setDataSource(rs.products.splice(0, 3));
             setLoading(false);
         })
     }, [])
@@ -92,6 +116,40 @@ function RecentOrders() {
             pagination={false}
         ></Table>
     </>
+}
+
+function DasboardChart() {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Order Revenue',
+            },
+        },
+    };
+
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: labels.map(() => Math.random()*1000),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Dataset 2',
+                data: labels.map(() => Math.random()*1000),
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+    return <Bar options={options} data={data} />;
 }
 
 export default Dashboard;
