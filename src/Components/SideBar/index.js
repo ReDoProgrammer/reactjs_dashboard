364552e6579c015cbdf190292928ec9be1751d19
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 12:53:30 +0700

    desgin menu

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,5 +1,31 @@
+import { Menu } from 'antd';
 function SideBar() {
-    return (  <div className="SideBar">Sidebar</div>);
+    return (<div className="SideBar">
+        <Menu 
+        onClick={item=>{
+            //item.key
+        }}
+        items={[
+            {
+                label: 'Dashboard',
+                key: '/'
+            },
+            {
+                label: 'Inventoy',
+                key: '/inventory'
+            },
+            {
+                label: 'Orders',
+                key: '/orders'
+            },
+            {
+                label: 'Customers',
+                key: '/customers'
+            }
+        ]}>
+
+        </Menu>
+    </div>);
 }
 
 export default SideBar;
