504d58e9fbd30dd0cf88fc85326345ee98233f50
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 13:11:59 +0700

    bind pagecontent following routes

--- src/Components/PageContent/index.js
+++ src/Components/PageContent/index.js
@@ -1,5 +1,9 @@
+import AppRoutes from "../AppRoutes";
+
 function PageContent() {
-    return (  <div className="PageContent">Content</div>);
+    return (  <div className="PageContent">
+        <AppRoutes/>
+    </div>);
 }
 
 export default PageContent;
