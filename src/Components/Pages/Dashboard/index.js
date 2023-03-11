d0f3c60b546ad49b1661450c6ac1e16d42de5d11
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 15:50:50 +0700

    surround dashboard with Space tag to style it

--- src/Components/Pages/Dashboard/index.js
+++ src/Components/Pages/Dashboard/index.js
@@ -5,45 +5,47 @@ import { getRecentOrders } from '../../../API'
 import { useEffect, useState } from 'react';
 function Dashboard() {
     return (<div>
-        <Typography.Title level={4}>Dashboard</Typography.Title>
-        <Space direction='horizontal'>
-            <DashboardCard title={'Orders'}
-                value={12345}
-                icon={<ShoppingCartOutlined
+        <Space size={20} direction="vertical">
+            <Typography.Title level={4}>Dashboard</Typography.Title>
+            <Space direction='horizontal'>
+                <DashboardCard title={'Orders'}
+                    value={12345}
+                    icon={<ShoppingCartOutlined
+                        style={{
+                            color: 'green',
+                            backgroundColor: 'rgba(0,255,0,0.25)',
+                            borderRadius: 20,
+                            fontSize: 24,
+                            padding: 8
+                        }}
+                    />}
+                />
+                <DashboardCard title={'Customers'} value={532} icon={<UserOutlined
                     style={{
-                        color: 'green',
-                        backgroundColor: 'rgba(0,255,0,0.25)',
+                        color: 'purple',
+                        backgroundColor: 'rgba(0,255,255,0.25)',
                         borderRadius: 20,
                         fontSize: 24,
                         padding: 8
-                    }}
-                />}
-            />
-            <DashboardCard title={'Customers'} value={532} icon={<UserOutlined
-                style={{
-                    color: 'purple',
-                    backgroundColor: 'rgba(0,255,255,0.25)',
+                    }} />} />
+                <DashboardCard title={'Inventory'} value={53632} icon={<ShoppingFilled style={{
+                    color: 'blue',
+                    backgroundColor: 'rgba(0,0,255,0.25)',
                     borderRadius: 20,
                     fontSize: 24,
                     padding: 8
                 }} />} />
-            <DashboardCard title={'Inventory'} value={53632} icon={<ShoppingFilled style={{
-                color: 'blue',
-                backgroundColor: 'rgba(0,0,255,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-            }} />} />
-            <DashboardCard title={'Revenue'} value={3563} icon={<DollarCircleOutlined style={{
-                color: 'red',
-                backgroundColor: 'rgba(255,0,0,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-            }} />} />
-        </Space>
-        <Space>
-            <RecentOrders />
+                <DashboardCard title={'Revenue'} value={3563} icon={<DollarCircleOutlined style={{
+                    color: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.25)',
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8
+                }} />} />
+            </Space>
+            <Space>
+                <RecentOrders />
+            </Space>
         </Space>
     </div>);
 }
