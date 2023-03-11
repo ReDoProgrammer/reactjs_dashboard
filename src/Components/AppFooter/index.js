d97b3ded9d1e5a197293e358d36e3328f462f193
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 12:49:48 +0700

    design footer

--- src/Components/AppFooter/index.js
+++ src/Components/AppFooter/index.js
@@ -1,5 +1,10 @@
+import {Typography} from 'antd';
 function AppFooter() {
-    return (  <div className="AppFooter">Footer</div>);
+    return (  <div className="AppFooter">
+        <Typography.Link href='tel:+84911397764'>+84911397764</Typography.Link>
+        <Typography.Link href='https://www.facebook.com/' target={'_blank'}>Private Policy</Typography.Link>
+        <Typography.Link href='https://www.facebook.com/' target={'_blank'}>Terms Of Use</Typography.Link>
+    </div>);
 }
 
 export default AppFooter;
