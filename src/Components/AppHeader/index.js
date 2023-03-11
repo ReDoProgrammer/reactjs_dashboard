5cae90c01b0201d9e513f46d4e87b06ca80d2eae
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 12:41:01 +0700

    design appheader

--- src/Components/AppHeader/index.js
+++ src/Components/AppHeader/index.js
@@ -1,5 +1,18 @@
+import { Image, Typography, Space,Badge } from 'antd';
+import { MailOutlined, BellFilled } from '@ant-design/icons';
 function AppHeader() {
-    return (  <div className="AppHeader">Header</div>);
+    return (<div className="AppHeader">
+        <Image src='https://cdn-icons-png.flaticon.com/128/1794/1794749.png' width={40} />
+        <Typography.Title>ReDo's Dashboard</Typography.Title>
+        <Space>
+            <Badge count = {5} dot>
+                <MailOutlined style={{ fontSize: 24 }} />
+            </Badge>
+            <Badge count={20}>
+                <BellFilled style={{ fontSize: 24 }} />
+            </Badge>
+        </Space>
+    </div>);
 }
 
 export default AppHeader;
