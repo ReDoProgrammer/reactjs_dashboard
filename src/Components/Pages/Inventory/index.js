fc19b6a44a5d77ce8b0ab6f9fd5ce8943f20690e
redoprogrammer <redoprogrammer@gmail.com>
2023-03-11 21:50:06 +0700

    get inventory with all products via API

--- src/Components/Pages/Inventory/index.js
+++ src/Components/Pages/Inventory/index.js
@@ -1,8 +1,55 @@
-import {Typography} from 'antd';
+import { Table, Typography,Space } from 'antd';
+import { getInventory } from '../../../API';
+import {useState, useEffect } from 'react';
 function Inventory() {
-    return (  <div>
+    const [loading, setLoading] = useState(false);
+    const [dataSource, setDataSource] = useState([]);
+    useEffect(() => {
+        setLoading(true);
+        getInventory()
+            .then(res => {
+                setLoading(false);
+                setDataSource(res.products);
+            })
+    }, [])
+
+    return (<Space size={20} direction="vertical">
         <Typography.Title level={4}>Inventory</Typography.Title>
-    </div>);
+        <Table
+            columns={[
+                {
+                    label: 'Title',
+                    dataIndex: 'title'
+                },
+                {
+                    label: 'Price',
+                    dataIndex: 'price'
+                },
+                {
+                    label: 'Rating',
+                    dataIndex: 'rating'
+                },
+                {
+                    label: 'Stock',
+                    dataIndex: 'stock'
+                },
+                {
+                    label: 'Thumbnail',
+                    dataIndex: 'thumbnail'
+                },
+                {
+                    label: 'Brand',
+                    dataIndex: 'brand'
+                },
+                {
+                    label: 'Category',
+                    dataIndex: 'category'
+                }
+            ]}
+            dataSource={dataSource}
+            loading={loading}
+        ></Table>
+    </Space>);
 }
 
 export default Inventory;
